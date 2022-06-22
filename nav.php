e2b3246e568cb2a5c620e34aa2297c61b4dedd75
a.malachowski <75166264+amalachowski@users.noreply.github.com>
2022-06-22 22:07:44 +0200

    dodanie kodu html

--- nav.php
+++ nav.php
@@ -19,7 +19,7 @@
                         <a class="nav-link text-dark fw-bold" href="#">O Naszej szkole</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark fw-bold" href="#">Kontakt</a>
+                        <a class="nav-link text-dark fw-bold" href="contact.php">Kontakt</a>
                     </li>
                 </ul>
                 <a class="btn btn-primary" href="login.php">Zaloguj</a>
