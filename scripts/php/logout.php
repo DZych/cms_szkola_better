98d5ccb8fdc9891ed3ca55a238eb8480d6d41a6c
Dominik Zych <DZych@protonmail.com>
2022-06-26 23:22:36 +0200

    wycięcie sidebara + dodanie opcji w zależności od użytkownika
    wydzielenie sidebara + ładowanie opcji w menu w zależności od typu użytkownika

--- scripts/php/logout.php
+++ scripts/php/logout.php
@@ -3,6 +3,9 @@ session_start();
 $_SESSION['user_id']="";
 $_SESSION['first_name']="";
 $_SESSION['last_name']="";
+$_SESSION['is_admin'] = false;
+$_SESSION['is_teacher'] = false;
+$_SESSION['is_student'] = false;
 session_destroy();
 header("location:../../login.php");
 ?>
--- scripts/php/zaloguj.php
+++ scripts/php/zaloguj.php
@@ -27,14 +27,39 @@
         $query = "UPDATE ".$prefix."_users SET `last_login_date` = current_timestamp(), `last_login_ip` = '".$_SERVER['REMOTE_ADDR']."' WHERE `user_id` = '".$wiersz['user_id']."';";
         mysqli_query($link, $query) or die ("Zapytanie zakończone niepowodzeniem");
 
+
+        set_type_user("admins", "is_admin");
+        set_type_user("teachers", "is_teacher");
+        set_type_user("students", "is_student");
+        
         unset($_SESSION['error']);
         header("location:../../dashboard/index.php");
         }
+        else{
+            $_SESSION['zalogowany'] = false;
+            $_SESSION['error'] = '<div class="alert alert-danger" role="alert">
+            Użytkownik nie istnieje!
+            </div>';
+          header("location:../../login.php");
+        }
     }
     else{
+        $_SESSION['zalogowany'] = false;
         $_SESSION['error'] = '<div class="alert alert-danger" role="alert">
         Użytkownik nie istnieje!
         </div>';
       header("location:../../login.php");
+    }
+      
+    function set_type_user($table_name, $value_name){
+        include("../../config.php");
+        $query = "SELECT * FROM ".$prefix."_".$table_name." WHERE user_id =".$_SESSION['user_id'].";";
+        $result = mysqli_query($link, $query) or die ("Zapytanie zakończone niepowodzeniem");
+        if(mysqli_num_rows($result)==1){
+            $_SESSION[$value_name] = true;
+        }
+        else{
+            $_SESSION[$value_name] = false;
+        }
     }
 ?>
