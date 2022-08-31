e3baa91bcc9e8b3922c97eba806b2f2ec7e87821
Łukasz Łągwa <84438057+LLagwa@users.noreply.github.com>
2022-08-31 15:52:18 +0200

    o naszej szkole
    o szkole

--- nav.php
+++ nav.php
@@ -1,28 +1,28 @@
  <!-- Nawigacja -->
  <nav class="navbar navbar-expand-sm navbar-light bg-white sticky-top shadow-5-stron shadow ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <img src="assets/img/logo.png" alt="Avatar Logo" style="width:40px;">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="main-navbar">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link text-dark fw-bold" href="#">Aktualności</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark fw-bold" href="#">Dziennik</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark fw-bold" href="#">O Naszej szkole</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark fw-bold" href="contact.php">Kontakt</a>
-                    </li>
-                </ul>
-                <a class="btn btn-primary" href="login.php">Zaloguj</a>
-            </div>
-        </div>
-    </nav>
+     <div class="container-fluid">
+         <a class="navbar-brand" href="index.php">
+             <img src="assets/img/logo.png" alt="Avatar Logo" style="width:40px;">
+         </a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="main-navbar">
+             <ul class="navbar-nav me-auto">
+                 <li class="nav-item">
+                     <a class="nav-link text-dark fw-bold" href="#">Aktualności</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link text-dark fw-bold" href="#">Dziennik</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link text-dark fw-bold" href="about.php">O Naszej szkole</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link text-dark fw-bold" href="contact.php">Kontakt</a>
+                 </li>
+             </ul>
+             <a class="btn btn-primary" href="login.php">Zaloguj</a>
+         </div>
+     </div>
+ </nav>
